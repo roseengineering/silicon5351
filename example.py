1a7fe6ab7dda3493f0c01e5599f27d5e40232f90
george <george>
2023-09-04 18:44:54 -0400

    add example

---
+++ example.py
@@ -0,0 +1,23 @@
+from silicon5351 import SI5351_CRYSTAL_LOAD_10PF, SI5351_I2C
+import machine 
+
+# i2c
+sda = machine.Pin(6)
+scl = machine.Pin(7)
+i2c = machine.I2C(1, sda=sda, scl=scl)
+
+crystal = 25e6     # crystal frequency
+mult = 32          # 32 * 25e6 = 800 MHz PLL frequency
+freq = 6.30e6      # frequency to output
+quadrature = True  # frequency limited to 800MHz/128
+
+# si5351
+load = SI5351_CRYSTAL_LOAD_10PF
+si = SI5351_I2C(i2c, crystal=crystal, load=load)
+
+si.init_clock(output=0, pll=0)
+si.init_clock(output=1, pll=0, quadrature=quadrature)
+si.setup_pll(pll=0, mult=mult)
+si.set_freq(output=0, freq=freq) 
+si.set_freq(output=1, freq=freq) 
+si.enable_outputs(0x3)
--- silicon5351.py
+++ silicon5351.py
@@ -211,18 +211,21 @@ if __name__ == '__main__':
     scl = machine.Pin(7)
     i2c = machine.I2C(1, sda=sda, scl=scl)
 
-    # si5351
-    crystal = 25e6
-    load = SI5351_CRYSTAL_LOAD_10PF
-    si = SI5351_I2C(i2c, crystal=crystal, load=load)
+    crystal = 25e6     # crystal frequency
+    mult = 32          # 32 * 25e6 = 800 MHz PLL frequency
+    freq = 6.30e6      # frequency to output
+    quadrature = True  # frequency limited to 800MHz/128
 
-    mult = 32
     freq = 6.251e6  # divisor = 127.9
     freq = 6.2987e6 # divisor = 127.01
     freq = 6.30e6   # divisor = 126.9
-    quadrature = True
+
+    # si5351
+    load = SI5351_CRYSTAL_LOAD_10PF
+    si = SI5351_I2C(i2c, crystal=crystal, load=load)
+
     si.init_clock(output=0, pll=0)
-    si.init_clock(output=1, pll=0, quadrature=True)
+    si.init_clock(output=1, pll=0, quadrature=quadrature)
     # si.init_clock(output=1, pll=0, invert=True)
     si.setup_pll(pll=0, mult=mult)
     si.set_freq(output=0, freq=freq) 
