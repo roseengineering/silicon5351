49f1c2ba7ecf3994f839e3b36adb0af7743ee974
george <george>
2023-09-22 20:14:00 -0400

    remove example from library code.

--- silicon5351.py
+++ silicon5351.py
@@ -210,32 +210,3 @@ class SI5351_I2C:
         self.write_config(reg, div, num, denom, rdiv=rdiv)
 
 
-if __name__ == '__main__':
-    # XIAO RP2040
-    if sys.implementation.name == 'circuitpython':
-        import board, busio
-        i2c = busio.I2C(board.SCL, board.SDA)
-        while not i2c.try_lock():
-            pass
-    else:
-        import machine
-        sda = machine.Pin(6)
-        scl = machine.Pin(7)
-        i2c = machine.I2C(1, sda=sda, scl=scl)
-
-    crystal = 25e6     # crystal frequency
-    mult = 32          # 32 * 25e6 = 800 MHz PLL frequency
-    freq = 6.30e6      # frequency to output
-    quadrature = True  # frequency limited to 800MHz/128
-
-    si = SI5351_I2C(i2c, crystal=crystal)
-    si.init_clock(output=0, pll=0)
-    si.init_clock(output=1, pll=0, quadrature=quadrature)
-    # si.init_clock(output=1, pll=0, invert=True)
-    si.setup_pll(pll=0, mult=mult)
-    si.set_freq(output=0, freq=freq) 
-    si.set_freq(output=1, freq=freq) 
-    si.enable_outputs(0x3)
-    print('done')
-
-
